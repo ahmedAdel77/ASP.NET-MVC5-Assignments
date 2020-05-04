42f28868883eb149ef7bbdf4bb73f0c87aca0e5e
Ahmed Adel <ahmedadelguy77@gmail.com>
2020-05-04 08:51:34 +0200

    Final Day01 Task

---
+++ Day01/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day01.Controllers
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ViewResult Register()
+        {
+            return View("RegisterationForm");
+        }
+
+        [HttpPost]
+        public ViewResult Register(Person person)
+        {
+            if (person != null && person.Name != null && person.Email != null)
+            {
+                ViewBag.Name = person.Name;
+                return View("Welcome");
+            }
+            return View("RegisterationForm");
+        }
+
+        [HttpGet]
+        public ViewResult Welcome()
+        {
+            return View();
+        }
+    }
+}
